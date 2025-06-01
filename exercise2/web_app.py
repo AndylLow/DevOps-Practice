47a26a25c6b3fee4e2fa5a150681ba641a21d2fb
YEmreSancak <yunus.emre.sancak99@gmail.com>
2025-06-01 16:26:49 +0300

    fix linting

--- exercise2/web_app.py
+++ exercise2/web_app.py
@@ -1,14 +1,30 @@
-# app.py
+"""
+Simple Flask web server that outputs the port it is running on.
+
+The port can be specified using the PORT environment variable.
+If not specified, it defaults to 5000.
+"""
+
 import os
 from flask import Flask
 
 app = Flask(__name__)
 
-PORT = int(os.environ.get("PORT", 5000))  # default to 5000
+# Default port if not specified by environment variable
+DEFAULT_PORT = 5000
+PORT = int(os.environ.get("PORT", DEFAULT_PORT))
+
 
 @app.route('/')
 def index():
+    """
+    Root route that returns the current running port as a string.
+
+    Returns:
+        str: Message stating the server's running port.
+    """
     return f"Server started in port {PORT}"
+
 
 if __name__ == '__main__':
     print(f"Server started in port {PORT}")
